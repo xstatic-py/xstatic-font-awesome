84a540bb0d2f17d62fa061ed28ecac3781b6e42e
ReimarBauer <rb.proj@gmail.com>
2023-01-18 16:02:39 +0100

    added xstatic repo url

--- xstatic/pkg/font_awesome/__init__.py
+++ xstatic/pkg/font_awesome/__init__.py
@@ -30,6 +30,9 @@ MAINTAINER_EMAIL = 'rb.proj@gmail.com'
 # this refers to the project homepage of the stuff we packaged:
 HOMEPAGE = 'https://github.com/FortAwesome/Font-Awesome'
 
+# this refers to the location of the xstatic package
+REPOSITORY = 'https://github.com/xstatic-py/xstatic-font-awesome'
+
 # this refers to all files:
 LICENSE = '(same as %s)' % DISPLAY_NAME
 
